bca793bfe3bf5f43c0596b2197f08d8541d2d628
nwroot <me@nwroot.xyz>
2024-03-11 20:32:27 -0300

    Implemented rank_support

--- python/src/rank_support.cpp
+++ python/src/rank_support.cpp
@@ -5,12 +5,14 @@ void _make_binding_rank_support(py::module &m, std::string typestr) {
     std::string class_name = std::string("rank_support_") + typestr;
     py::class_<Class>(m, class_name.c_str())
     
-    .def(py::init<sdsl::bit_vector>())
+    .def(py::init<sdsl::bit_vector *>())
     
     .def("rank", &Class::rank)
     .def("__call__", &Class::operator());
 }
 
 void make_binding_rank_support(py::module &m) {
-    // TODO
+    _make_binding_rank_support<sdsl::rank_support_v<>>(m, "v");
+    _make_binding_rank_support<sdsl::rank_support_v5<>>(m, "v5");
+    _make_binding_rank_support<sdsl::rank_support_scan<>>(m, "scan");
 }
